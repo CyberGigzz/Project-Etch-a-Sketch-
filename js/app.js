5097b2343241cbf0e2fc5e12290a1beec4096301
Giga_Dev148 <gigakatamadze0@gmail.com>
2023-05-31 18:00:42 +0400

    Rewriting the logic

--- js/app.js
+++ js/app.js
@@ -1,17 +1,25 @@
+const DEFAULT_COLOR = '#333333'
+const DEFAULT_MODE = 'color'
+const DEFAULT_SIZE = 16
+
+let currentColor = DEFAULT_COLOR
+let currentMode = DEFAULT_MODE
+let currentSize = DEFAULT_SIZE
 
 
-
+const colorPicker = document.getElementById("colorPicker");
+const colorBtn = document.querySelector(".color_mode");
+const rainbowColorBtn = document.querySelector(".rainbow_mode");
+const eraserBtn = document.querySelector(".eraser");
+const clear = document.querySelector(".clear");
 
 
 
 
 const board = document.querySelector(".board");
-const defaultSize = 16;
 const rangeInput = document.querySelector("#range");
 const sizeValue = document.querySelector("#sizeValue");
 // Color mode
-const colorMode = document.querySelector(".color_mode");
-const colorPicker = document.getElementById("colorPicker");
 colorPicker.value = "#ffffff"; // Set the default color to white
 // App starts with a size of 16 X 16
 
